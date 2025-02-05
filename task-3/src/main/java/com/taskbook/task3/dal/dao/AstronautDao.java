7b03bbb2e35b1bd3cbf921395f9696fe98e21333
gor1sher <164029653+gor1sher@users.noreply.github.com>
2025-02-05 20:23:12 +0300

    тз

--- task-3/src/main/java/com/taskbook/task3/dal/dao/AstronautDao.java
+++ task-3/src/main/java/com/taskbook/task3/dal/dao/AstronautDao.java
@@ -1,12 +1,15 @@
 package com.taskbook.task3.dal.dao;
 
 import com.taskbook.task3.dal.entities.Astronaut;
+import com.taskbook.task3.exception.AstronautNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Slf4j
 @Repository
 public class AstronautDao {
 
@@ -27,7 +30,9 @@ public class AstronautDao {
     }
 
     public Astronaut findById(Long id) {
-        return null;
+        try (var session = sessionFactory.openSession()) {
+            return session.get(Astronaut.class, id);
+        }
     }
 
     public void insertData(Astronaut astronaut) {
@@ -44,10 +49,32 @@ public class AstronautDao {
     }
 
     public void updateData(Astronaut astronaut) {
-
+        try (var session = sessionFactory.openSession()) {
+            var transaction = session.beginTransaction();
+            try {
+                session.update(astronaut);
+                transaction.commit();
+            } catch (Exception ex) {
+                transaction.rollback();
+                throw new RuntimeException(ex.getMessage());
+            }
+        }
     }
 
     public void deleteData(Long id) {
-
+        try (var session = sessionFactory.openSession()) {
+            var transaction = session.beginTransaction();
+            try {
+                var astronaut = session.get(Astronaut.class, id);
+                if (astronaut == null){
+                    throw new AstronautNotFoundException("такого объекта нет");
+                }
+                session.delete(astronaut);
+                transaction.commit();
+            } catch (Exception ex) {
+                transaction.rollback();
+                throw new RuntimeException(ex.getMessage());
+            }
+        }
     }
 }
